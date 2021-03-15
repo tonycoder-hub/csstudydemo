e191289109929dbc4b2d8e9d7121d55c28d2282b
Tony <407243179@qq.com>
2021-03-15 12:51:45 +0800

    Console App Done

--- ConsoleAppDemo/ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/ConsoleAppDemo/Program.cs
@@ -6,7 +6,39 @@ namespace ConsoleAppDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Give me 2 numbers and a signal");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            char c = Convert.ToChar(Console.ReadLine());
+            switch (c)
+            {
+                case '+':
+                    {
+                        Console.WriteLine(a + b);
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine(a - b);
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine(a * b);
+                        break;
+                    }
+                case '/':
+                    {
+                        Console.WriteLine(a / b);
+                        break;
+                    }
+                case '%':
+                    {
+                        Console.WriteLine(a % b);
+                        break;
+                    }
+            }
+            
         }
     }
 }

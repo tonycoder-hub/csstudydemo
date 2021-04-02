70c4deb5ff8faf2a513bb05f5225d3b8432e0ed0
Tony Coder's Surface <407243179@qq,com>
2021-04-02 12:23:15 +0800

    Mar 19th. finished.

--- WinFormsAppMar19th/WinFormsAppMar19th/WinFormsAppMar19th/Form1.Designer.cs
+++ WinFormsAppMar19th/WinFormsAppMar19th/WinFormsAppMar19th/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace WinFormsAppMar19th
 {
     partial class Form1
@@ -30,12 +32,95 @@ namespace WinFormsAppMar19th
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(219, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Expected Time(second):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(219, 198);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tick";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Not now";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(422, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+          
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1354, 828);
+            
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Alarm & Tick";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+       
+
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- WinFormsAppMar19th/WinFormsAppMar19th/WinFormsAppMar19th/Form1.cs
+++ WinFormsAppMar19th/WinFormsAppMar19th/WinFormsAppMar19th/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,10 +13,42 @@ namespace WinFormsAppMar19th
 {
     public partial class Form1 : Form
     {
+       
+        int count,num;
+        
         public Form1()
         {
             InitializeComponent();
+    
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num =Convert.ToInt32( this.textBox1.Text);
+            this.timer1.Interval = 1000;
+            timer1.Start();
+            this.label2.Text = "starts....";
+        }
+       
+    private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            count++;
+            this.label2.Text = (num-count).ToString()+" second(s) left.";
+            if (count == num)
+            {
+                this.label2.Text = "Ring Ring Ring~~~";
+                timer1.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.label2.Text = "Refreshed.";
+            timer1.Stop();
+        }
+
+        
+       
     }
 }
